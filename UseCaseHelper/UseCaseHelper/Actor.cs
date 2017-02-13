3fa40e05c9b6bb76772e42d74899b17b4df50b2a
ruudschouten <ruud.schouten@outlook.com>
2017-02-13 15:02:13 +0100

    Created class for Actor

---
+++ UseCaseHelper/UseCaseHelper/Actor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCaseHelper {
+    public class Actor {
+        public string Naam { get; set; }
+        public Point Position { get; set; }
+
+        public Actor() { }
+
+        public Actor(string naam, Point position) {
+            Naam = naam;
+            Position = position;
+        }
+
+        public void Paint(Graphics g) {
+
+        }
+    }
+}
