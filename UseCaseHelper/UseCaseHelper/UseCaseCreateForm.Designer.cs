0028def381961564f406bf077a870635de5ddd03
ruudschouten <ruud.schouten@outlook.com>
2017-02-13 15:22:04 +0100

    Created UI which is used to add new elements to the canvas.

---
+++ UseCaseHelper/UseCaseHelper/UseCaseCreateForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace UseCaseHelper {
+    partial class UseCaseCreateForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbResultaat = new System.Windows.Forms.TextBox();
+            this.lbResultaat = new System.Windows.Forms.Label();
+            this.rtbUitzonderingen = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rtbBeschrijving = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbAannamen = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbActoren = new System.Windows.Forms.TextBox();
+            this.tbSamenvatting = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNaam = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbResultaat
+            // 
+            this.tbResultaat.Location = new System.Drawing.Point(102, 295);
+            this.tbResultaat.Name = "tbResultaat";
+            this.tbResultaat.Size = new System.Drawing.Size(172, 20);
+            this.tbResultaat.TabIndex = 34;
+            // 
+            // lbResultaat
+            // 
+            this.lbResultaat.AutoSize = true;
+            this.lbResultaat.Location = new System.Drawing.Point(14, 302);
+            this.lbResultaat.Name = "lbResultaat";
+            this.lbResultaat.Size = new System.Drawing.Size(52, 13);
+            this.lbResultaat.TabIndex = 33;
+            this.lbResultaat.Text = "Resultaat";
+            // 
+            // rtbUitzonderingen
+            // 
+            this.rtbUitzonderingen.Location = new System.Drawing.Point(102, 209);
+            this.rtbUitzonderingen.Name = "rtbUitzonderingen";
+            this.rtbUitzonderingen.Size = new System.Drawing.Size(172, 80);
+            this.rtbUitzonderingen.TabIndex = 32;
+            this.rtbUitzonderingen.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Uitzonderingen";
+            // 
+            // rtbBeschrijving
+            // 
+            this.rtbBeschrijving.Location = new System.Drawing.Point(102, 123);
+            this.rtbBeschrijving.Name = "rtbBeschrijving";
+            this.rtbBeschrijving.Size = new System.Drawing.Size(172, 80);
+            this.rtbBeschrijving.TabIndex = 30;
+            this.rtbBeschrijving.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Beschrijving";
+            // 
+            // tbAannamen
+            // 
+            this.tbAannamen.Location = new System.Drawing.Point(102, 90);
+            this.tbAannamen.Name = "tbAannamen";
+            this.tbAannamen.Size = new System.Drawing.Size(172, 20);
+            this.tbAannamen.TabIndex = 28;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Aannamen";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Location = new System.Drawing.Point(14, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Actoren";
+            // 
+            // tbActoren
+            // 
+            this.tbActoren.Enabled = false;
+            this.tbActoren.Location = new System.Drawing.Point(102, 64);
+            this.tbActoren.Name = "tbActoren";
+            this.tbActoren.Size = new System.Drawing.Size(172, 20);
+            this.tbActoren.TabIndex = 25;
+            // 
+            // tbSamenvatting
+            // 
+            this.tbSamenvatting.Location = new System.Drawing.Point(102, 38);
+            this.tbSamenvatting.Name = "tbSamenvatting";
+            this.tbSamenvatting.Size = new System.Drawing.Size(172, 20);
+            this.tbSamenvatting.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Samenvatting";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Naam";
+            // 
+            // tbNaam
+            // 
+            this.tbNaam.Location = new System.Drawing.Point(102, 12);
+            this.tbNaam.Name = "tbNaam";
+            this.tbNaam.Size = new System.Drawing.Size(172, 20);
+            this.tbNaam.TabIndex = 21;
+            // 
+            // UseCaseCreateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 332);
+            this.Controls.Add(this.tbResultaat);
+            this.Controls.Add(this.lbResultaat);
+            this.Controls.Add(this.rtbUitzonderingen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rtbBeschrijving);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbAannamen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbActoren);
+            this.Controls.Add(this.tbSamenvatting);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNaam);
+            this.Name = "UseCaseCreateForm";
+            this.Text = "UseCaseCreateForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbResultaat;
+        private System.Windows.Forms.Label lbResultaat;
+        private System.Windows.Forms.RichTextBox rtbUitzonderingen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox rtbBeschrijving;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbAannamen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbActoren;
+        private System.Windows.Forms.TextBox tbSamenvatting;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNaam;
+    }
+}
---
+++ UseCaseHelper/UseCaseHelper/UseCaseCreateForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseCaseHelper {
+    public partial class UseCaseCreateForm : Form {
+        private UseCase useCase;
+        public UseCaseCreateForm() {
+            InitializeComponent();
+        }
+
+        public UseCaseCreateForm(UseCase u) : this() {
+            useCase = u;
+        }
+    }
+}
+
