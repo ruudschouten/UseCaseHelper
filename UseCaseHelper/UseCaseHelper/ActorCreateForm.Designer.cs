65e3e37f50766edec717dd835faf42e596e50dcf
ruudschouten <ruud.schouten@outlook.com>
2017-02-14 10:41:56 +0100

    Added a form to create an Actor.

---
+++ UseCaseHelper/UseCaseHelper/ActorCreateForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace UseCaseHelper {
+    partial class ActorCreateForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.tbNaam = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Naam";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(142, 35);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // tbNaam
+            // 
+            this.tbNaam.Location = new System.Drawing.Point(53, 9);
+            this.tbNaam.Name = "tbNaam";
+            this.tbNaam.Size = new System.Drawing.Size(164, 20);
+            this.tbNaam.TabIndex = 2;
+            this.tbNaam.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbNaam_KeyDown);
+            // 
+            // ActorCreateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 71);
+            this.Controls.Add(this.tbNaam);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label1);
+            this.Name = "ActorCreateForm";
+            this.Text = "ActorCreateForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ActorCreateForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox tbNaam;
+    }
+}
---
+++ UseCaseHelper/UseCaseHelper/ActorCreateForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseCaseHelper {
+    public partial class ActorCreateForm : Form {
+        private Actor actor;
+        private Point position;
+        public ActorCreateForm() {
+            InitializeComponent();
+            ActiveControl = tbNaam;
+        }
+
+        public ActorCreateForm(Actor a) : this() {
+            actor = a;
+            tbNaam.Text = a.Naam;
+            position = actor.Position;
+        }
+
+        public ActorCreateForm(Actor a, Point position) : this() {
+            actor = a;
+            this.position = position;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void ActorCreateForm_FormClosing(object sender, FormClosingEventArgs e) {
+            e.Cancel = true;
+            if (string.IsNullOrEmpty(tbNaam.Text)) {
+                var messageResult = MessageBox.Show("Naam is niet ingevuld, wilt u toch afsluiten?", "Onopgeslagen aanpassingein!", MessageBoxButtons.YesNo);
+                switch (messageResult) {
+                    case DialogResult.None:
+                    case DialogResult.No:
+                        e.Cancel = true;
+                        break;
+                    case DialogResult.Yes:
+                        e.Cancel = false;
+                        break;
+                }
+            }
+            actor = new Actor(tbNaam.Text, position);
+            e.Cancel = false;
+        }
+
+        public Actor GetActor() {
+            return actor;
+        }
+
+        private void tbNaam_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyData == Keys.Enter) {
+                Close();
+            }
+        }
+    }
+}
